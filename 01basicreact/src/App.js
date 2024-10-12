69d0dfcecb50e5a342186bd66877a7ec68861d25
Sumit Sharma <procodrr786@gmail.com>
2024-10-12 16:42:54 +0530

    Learn how to setup react using create-react-app and using vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur code | Sumit Sharma</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <h1>Chai aur code | Sumit Sharma</h1>
+  )
+}
+
+export default App
